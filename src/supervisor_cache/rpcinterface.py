35a1c980d5facfb248272412420b17467180fde5
Mike Naberezny <mike@maintainable.com>
2007-11-03 03:16:14 +0000

    Initial cache implementation.

--- src/supervisor_cache/rpcinterface.py
+++ src/supervisor_cache/rpcinterface.py
@@ -1,4 +1,5 @@
 import os
+import xmlrpclib
 
 from supervisor.options import UnhosedConfigParser
 from supervisor.options import ProcessGroupConfig
@@ -9,15 +10,13 @@ from supervisor.http import NOT_DONE_YET
 
 API_VERSION = '1.0'
 
-class Faults:
-    STILL_RUNNING = 220
-
 class CacheNamespaceRPCInterface:
     """ A supervisor rpc interface for caching """
 
     def __init__(self, supervisord):
         self.supervisord = supervisord
-
+        self.cache = {}
+        
     def _update(self, text):
         self.update_text = text # for unit tests, mainly
 
@@ -38,6 +37,78 @@ class CacheNamespaceRPCInterface:
         self._update('getAPIVersion')
         return API_VERSION
 
+    def getKeys(self):
+        """ Return keys for all data stored in the cache
+
+        @return array result An array of strings representing cache keys
+        """
+        self._update('getKeys')
+        
+        return self.cache.keys()
+
+    def store(self, key, data):
+        """ Store a cache value in 'key'
+
+        @param string key    A string to use as a cache key
+        @param string data   A string for cache value (may also be Binary)
+        @return boolean result  Always true unless error
+        """
+        self._update('store')
+        
+        if isinstance(data, xmlrpclib.Binary):
+            data = data.data
+
+        try:
+            key = str(key)
+            if key == '':
+                raise RPCError(SupervisorFaults.BAD_NAME)
+            self.cache[key] = data
+        except RPCError:
+            raise
+        except:
+            raise RPCError(SupervisorFaults.FAILED)
+
+        return True
+
+    def fetch(self, key):
+        """ Retrieve data from cache stored under 'key'
+
+        @param string key  The cache key
+        @return binary value  An xmlrpc Binary value
+        """
+        
+        self._update('fetch')
+
+        key = str(key)
+        data = self.cache.get(key)
+        if data is None:
+            raise RPCError(SupervisorFaults.BAD_NAME)
+        try:
+            return xmlrpclib.Binary(data)
+        except:
+            raise RPCError(SupervisorFaults.FAILED)
+
+    def delete(self, key):
+        """ Delete data stored in cache under 'key'
+
+        @param string key  The key to delete from the cache
+        @return boolean result  Always true unless error.
+        """
+        self._update('delete')
+
+        if self.cache.has_key(key):
+            del self.cache[key]
+        return True
+
+    def clear(self):
+        """ Clear the cache
+
+        @return boolean result  Always true unless error.
+        """
+        self._update('clear')
+
+        self.cache.clear()
+        return True
 
 def make_cache_rpcinterface(supervisord, **config):
     return CacheNamespaceRPCInterface(supervisord)
--- src/supervisor_cache/tests/test_rpcinterface.py
+++ src/supervisor_cache/tests/test_rpcinterface.py
@@ -1,34 +1,18 @@
 import sys
 import unittest
+import xmlrpclib
 
 import supervisor
 from supervisor.xmlrpc import Faults as SupervisorFaults
 from supervisor.supervisord import SupervisorStates
 
 import supervisor_cache
-from supervisor_cache.rpcinterface import Faults as CacheFaults
 
 from supervisor.tests.base import DummySupervisor
 from supervisor.tests.base import DummyPConfig, DummyProcess
 from supervisor.tests.base import DummyPGroupConfig, DummyProcessGroup
 
 class TestRPCInterface(unittest.TestCase):
-
-    # Fault Constants
-
-    def test_cache_fault_names_dont_clash_with_supervisord_fault_names(self):
-        supervisor_faults = self.attrDictWithoutUnders(SupervisorFaults)
-        cache_faults = self.attrDictWithoutUnders(CacheFaults)
-
-        for name in supervisor_faults.keys():
-            self.assertNone(cache_faults.get(name))
-
-    def test_cache_fault_codes_dont_clash_with_supervisord_fault_codes(self):
-        supervisor_fault_codes = self.attrDictWithoutUnders(SupervisorFaults).values()
-        cache_fault_codes = self.attrDictWithoutUnders(CacheFaults).values()
-
-        for code in supervisor_fault_codes:
-            self.assertFalse(code in cache_fault_codes)
 
     # Constructor
     
@@ -70,6 +54,94 @@ class TestRPCInterface(unittest.TestCase):
         from supervisor_cache.rpcinterface import API_VERSION
         self.assertEqual(version, API_VERSION)
 
+    # API Method cache.store()
+    
+    def test_store_raises_bad_name_when_key_is_invalid(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+
+        self.assertRPCError(SupervisorFaults.BAD_NAME,
+                    interface.store, '', 'data')
+    
+    def test_store(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+
+        interface.store('the-key', 'its-value')
+        self.assertEqual(interface.cache['the-key'], 'its-value')
+        self.assertEqual(interface.update_text, 'store')
+    
+    def test_store_handles_binary_values(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+
+        interface.store('the-key', xmlrpclib.Binary('its-binary-value'))
+        self.assert_(not isinstance(interface.cache['the-key'], xmlrpclib.Binary))
+        self.assertEqual(interface.cache['the-key'], 'its-binary-value')
+    
+    # API Method cache.getKeys()
+    
+    def test_getKeys_returns_empty_list_when_no_items_cache(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {}
+
+        self.assertEqual(interface.getKeys(), [])
+        self.assertEqual(interface.update_text, 'getKeys')
+
+    def test_getKeys_returns_list_of_keys_for_items_in_cache(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {'foo': None, 'bar': None}
+
+        self.assertEqual(interface.getKeys(), ['foo','bar'])
+        self.assertEqual(interface.update_text, 'getKeys')
+    
+    # API Method cache.fetch()
+    
+    def test_fetch_raises_bad_name_when_key_does_not_exist(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {}
+                
+        self.assertRPCError(SupervisorFaults.BAD_NAME, 
+                            interface.fetch, 'nonexistant-key')
+    
+    def test_fetch(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {'the-key': 'its-value'}
+
+        self.assertEqual('its-value', interface.fetch('the-key'))
+        self.assertEqual(interface.update_text, 'fetch')
+
+    # API Method cache.delete()
+    
+    def test_delete_fails_silently_when_key_does_not_exist(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {}
+
+        self.assertTrue(interface.delete('nonexistant-key'))
+
+    def test_delete_removes_data_from_cache_by_key(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {'delete-me': 'foo', 'keep-me': 'bar'}
+
+        self.assertTrue(interface.delete('delete-me'))
+        self.assertEqual({'keep-me': 'bar'}, interface.cache)
+    
+    # API Method cache.clear()
+    
+    def test_clear(self):
+        supervisord = DummySupervisor()
+        interface = self.makeOne(supervisord)
+        interface.cache = {'foo': 'bar'}
+        
+        interface.clear()
+        self.assertEqual({}, interface.cache)
+        self.assertEqual(interface.update_text, 'clear')
 
     # Helpers Methods
     
