b1b0c4ec0e948bd7f5e600ad226c5f265d663c8e
Mike Naberezny <mike@naberezny.com>
2009-04-19 18:37:56 -0700

    Added help for commands.  Fixed factory method signature.

--- src/supervisor_cache/controllerplugin.py
+++ src/supervisor_cache/controllerplugin.py
@@ -10,29 +10,48 @@ class CacheControllerPlugin(ControllerPluginBase):
         keys = self.cache.getKeys()
         self._pprint(keys)
 
+    def help_cache_keys(self):
+        self.ctl.output("cache_keys\t"
+                        "List the keys of all items in the cache.")
+
     def do_cache_fetch(self, args):
         value = self.cache.fetch(args)
         self._pprint(value)
 
+    def help_cache_fetch(self):
+        self.ctl.output("cache_fetch <key>\t"
+                        "Fetch the item cached at <key>.")
+
     def do_cache_store(self, args):
         splitted = args.split(None, 1)
         if len(splitted) != 2:
-            self.help_cache_store()
-            return
-            
+            return self.help_cache_store()
+
         key, value = splitted
         self.cache.store(key, value)        
 
+    def help_cache_store(self): 
+        self.ctl.output("cache_store <key> <value>\t"
+                        "Store <value> in the cache at <key>.")
+        
     def do_cache_delete(self, args):
         self.cache.delete(args)
 
+    def help_cache_delete(self):
+        self.ctl.output("cache_delete <key>\t"
+                        "Delete the item cached at <key>.")
+
     def do_cache_clear(self, args):
         self.cache.clear()
+
+    def help_cache_clear(self):
+        self.ctl.output("cache_clear\t"
+                        "Clear all items from the cache.")
 
     def _pprint(self, what):
         pprinted = pprint.pformat(what)
         self.ctl.output(pprinted)        
 
 
-def make_cache_controllerplugin(supervisord, **config):
-    return CacheControllerPlugin(supervisord)
+def make_cache_controllerplugin(controller, **config):
+    return CacheControllerPlugin(controller)
